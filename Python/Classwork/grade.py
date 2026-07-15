a9e0744b16ad84590bc6183fa209c01f238f1366
Dipesh Tambakhe <dipseditor@gmail.com>
2026-07-15 12:30:56 +0530

    All

---
+++ Python/Classwork/grade.py
@@ -0,0 +1,19 @@
+marks = int(input("Enter your marks:"))
+
+if marks >= 91 and marks <=100:
+    print("Your grade is A")
+
+elif marks >= 71 and marks <=90:
+    print("Your garde is B")
+
+elif marks >= 51 and marks <=70:
+    print("Your grade is C")
+
+elif marks >=35 and marks <=50:
+    print("Your grade is D")
+
+elif marks >=0 and marks <=34:
+    print("Your grade is F")
+
+else:
+    print("Invalid marks")
